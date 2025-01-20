cb40cd98f524fc09d51ea4517e2d7d96cac45ab7
derickp13 <business.winstonpeters@gmail.com>
2025-01-20 15:40:19 -0500

    Use our HCP Terraform organization

--- main.tf
+++ main.tf
@@ -11,10 +11,10 @@ provider "aws" {
 terraform {
 
   cloud {
-    organization = "FlemingFriday"
+    organization = "DerickLovesBunda"
 
     workspaces {
-      name = "learn-terraform-github-actions"
+      name = "learn-terraform-github-action"
     }
   }
 
